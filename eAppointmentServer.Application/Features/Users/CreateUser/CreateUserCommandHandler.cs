458a4258ad7bcc99664aa41481c2f289db62c198
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-06-23 17:20:40 +0300

    Kullanıcıya ait bilgiler CRUD operasyonları ile yönetiliyor. Kullanıcı için CRUD operasyonları CQRS ve Mediator tasarım şablonu kullanılarak tasarlandı böylelikle rahat yönetilebilir modüler hale getirildi.

---
+++ eAppointmentServer.Application/Features/Users/CreateUser/CreateUserCommandHandler.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using eAppointmentServer.Domain.Entities;
+using eAppointmentServer.Domain.Repositories;
+using GenericRepository;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using TS.Result;
+
+namespace eAppointmentServer.Application.Features.Users.CreateUser;
+
+internal sealed class CreateUserCommandHandler(
+    UserManager<AppUser> userManager,
+    IUserRoleRepository userRoleRepository,
+    IUnitOfWork unitOfWork,
+    IMapper mapper) : IRequestHandler<CreateUserCommand, Result<string>>
+{
+    public async Task<Result<string>> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+    {
+        if(await userManager.Users.AnyAsync(p=> p.Email == request.Email))
+        {
+            return Result<string>.Failure("E-posta adresi zaten var");
+        }
+
+        if (await userManager.Users.AnyAsync(p => p.UserName == request.UserName))
+        {
+            return Result<string>.Failure("Kullanıcı adı zaten var");
+        }
+
+        AppUser user = mapper.Map<AppUser>(request);
+        IdentityResult result = await userManager.CreateAsync(user, request.Password);
+        if(!result.Succeeded)
+        {
+            return Result<string>.Failure(result.Errors.Select(s => s.Description).ToList());
+        }
+
+
+        if (request.RoleIds.Any())
+        {
+            List<AppUserRole> userRoles = new();
+            foreach (var roleId in request.RoleIds)
+            {
+                AppUserRole userRole = new()
+                {
+                    RoleId = roleId,
+                    UserId = user.Id
+                };
+                userRoles.Add(userRole);
+            }
+
+            await userRoleRepository.AddRangeAsync(userRoles, cancellationToken);
+            await unitOfWork.SaveChangesAsync(cancellationToken);
+        }
+
+        return "Kullanıcı kaydı başarı ile yapıldı";
+    }
+}
